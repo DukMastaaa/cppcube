888c8e784169150bd81836fac9feb24534b7ca84
DukMastaaa <beans@beans.com>
2021-01-09 19:33:01 +1000

    beans

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <ncurses.h>
 #include <iostream>
+#include "cubes.h"
+#include "cubeView.h"
+#include "colours.h"
 
 int main() {
     initscr();
     noecho();
     cbreak();
     curs_set(0);
+    startColours();
+    /*
     int beforeX, beforeY;
     getmaxyx(stdscr, beforeY, beforeX);
     WINDOW* meow = newwin(5, 5, 0, 0);
@@ -17,5 +22,16 @@ int main() {
     std::cout << beforeX << ' ' << beforeY << '\n';
     std::cout << afterX << ' ' << afterY << '\n';
     std::cout << lolx << ' ' << loly << '\n';
+    */
+    CubeModel cube(7);
+    cube.parseMoves(std::string("R U R' U'"));
+    CubeView view(cube);
+    view.draw();
+    view.wnoutrefresh();
+    doupdate();
+
+    wgetch(view.viewWindow);
+    endwin();
+
     return 0;
 }
