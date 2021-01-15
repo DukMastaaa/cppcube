1d432aa27f2c22ff5395a6a39775465b75bb70d9
DukMastaaa <beans@beans.com>
2021-01-15 16:04:24 +1000

    timer work

--- src/model/cubeTimer.cpp
+++ src/model/cubeTimer.cpp
@@ -2,6 +2,34 @@
 #include "cubeTimer.h"
 
 
+CubeTimer::CubeTimer() {
+    isTiming = false;
+    // uh
+}
+
+
+void CubeTimer::resetTime() {
+    timeElapsed = std::chrono::milliseconds(0);
+}
+
+
+void CubeTimer::startTimingAndReset() {
+    isTiming = true;
+    resetTime();
+    startingTime = clock.now();
+}
+
+
+void CubeTimer::stopTiming() {
+    namespace chrono = std::chrono;
+    if (isTiming) {
+        isTiming = false;
+        endingTime = clock.now();
+        timeElapsed = chrono::duration_cast<chrono::milliseconds>(endingTime - startingTime);
+    }
+}
+
+
 std::chrono::milliseconds CubeTimer::getTimeElapsed() {
-    return std::chrono::milliseconds(754560);  // WIP
+    return timeElapsed;
 }
