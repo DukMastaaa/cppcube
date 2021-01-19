996d12641f2b7852cd8de04b0d8aab704edd68a2
DukMastaaa <beans@beans.com>
2021-01-19 23:10:20 +1000

    recordlist vm start

--- src/include/recordListViewModel.h
+++ src/include/recordListViewModel.h
@@ -1,11 +1,18 @@
+#include <stddef.h>
+
 #include "baseViewModel.h"
 #include "recordList.h"
+#include "myStructs.h"
 
 
 class RecordListViewModel : public BaseViewModel {
     private:
         RecordList& records;
+        int recordsShown;
+        std::size_t selectedRecordRevIdx;  // bad name
     
     public:
         RecordListViewModel(RecordList& recordsRef);
+        Pos2D calcHeightWidth();
+        void draw(WINDOW* window);
 };
