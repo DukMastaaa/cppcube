57f8e318f49151ab156cc794f2cfc47db12dfa86
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2021-02-20 14:32:24 +1000

    Revert "making things static??"
    This reverts commit 330bb0e7972a1ef4b39840b45517089bd1c7a082.

--- include/app.h
+++ include/app.h
@@ -17,57 +17,57 @@
 
 class App {
     private:
-        static CubeController cubeController;
-        static RecordListController recordListController;
-        static ScramblerController scramblerController;
-        static TimerController timerController;
+        CubeController cubeController;
+        RecordListController recordListController;
+        ScramblerController scramblerController;
+        TimerController timerController;
 
-        static std::vector<BaseController*> mainControllers;
-        static std::vector<std::pair<PopupCallback, std::unique_ptr<PopupControllerInterface>>> popupControllers;
- 
-        static int dim;  // todo: only temporary
-        static bool doAnUpdate;
-        static bool appRunning;
+        std::vector<BaseController*> mainControllers;
+        std::vector<std::pair<PopupCallback, std::unique_ptr<PopupControllerInterface>>> popupControllers;
+
+        int dim;  // todo: only temporary
+        bool doAnUpdate;
+        bool appRunning;
 
         // helper
-        static void refreshAllControllers();
+        void refreshAllControllers() const;
 
         // handles keyboard inputs for main window (no popups)
-        static void mainWindowKeyboardInput(int input);
+        void mainWindowKeyboardInput(int input);
 
         // app functionality
-        static void handleTerminalResize();
-        static void toggleTimer();
-        static void togglePenalty(Penalty penalty, std::size_t recordNum);
+        void handleTerminalResize();
+        void toggleTimer();
+        void togglePenalty(Penalty penalty, std::size_t recordNum);
 
-        static void moveSelectedRecordUp();
-        static void moveSelectedRecordDown();
-        static void moveSelectedRecordTop();
-        static void moveSelectedRecordBottom();
+        void moveSelectedRecordUp();
+        void moveSelectedRecordDown();
+        void moveSelectedRecordTop();
+        void moveSelectedRecordBottom();
 
-        static void jumpSelectedIndex(std::size_t index);
-        static void generateNewScramble();
+        void jumpSelectedIndex(std::size_t index);
+        void generateNewScramble();
 
         // popup callbacks
-        static void changeCubeDim(std::string popupReturnData);
+        void changeCubeDim(std::string popupReturnData);
 
     public:
-        // App(int cubeDim);
-        static void initialRefreshUpdate();
-        static void keyboardInput(int input);
-        static bool needUpdate();
-        static void turnOffUpdate();
-        static void forceUpdate();
-        static bool appIsRunning();
-        static WINDOW* getWindow();
+        App(int cubeDim);
+        void initialRefreshUpdate();
+        void keyboardInput(int input);
+        bool needUpdate() const;
+        void turnOffUpdate();
+        void forceUpdate();
+        bool appIsRunning() const;
+        WINDOW* getWindow() const;
 
         template<typename ViewModel, typename ModelClass, typename Window = CentredWindow>
-        static void createPopup(PopupCallback callback, ModelClass& modelRef);
+        void createPopup(PopupCallback callback, ModelClass& modelRef);
 
         template<typename ViewModel>
-        static void createPopup(PopupCallback callback);
+        void createPopup(PopupCallback callback);
 
-        static void sendDataToLatestPopup(std::string data);
+        void sendDataToLatestPopup(std::string data);
 };
 
 
